418f22b0cc9d9cf5510ac1339b922ec6918ef998
Jhefry09 <jhefrycabanillas@gmail.com>
2025-09-28 01:07:29 -0500

    cambios axel

--- src/main.tsx
+++ src/main.tsx
@@ -1,2 +1,13 @@
-import {ComponentPreviews, useInitial} from "@/dev";
-import {DevSupport} from "@react-buddy/ide-toolbox";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { VocalContextProvider } from './contexts/VocalContextProvider';
+
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <VocalContextProvider>
+            <App />
+        </VocalContextProvider>
+    </StrictMode>,
+);
